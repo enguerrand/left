4a0468ac41c15e9ad78a294f9e01281a29df13d4
edr <github@rochefort.de>
2023-05-03 22:26:12 +0200

    Fix update.sh

--- dynamic-assets/update.sh
+++ dynamic-assets/update.sh
@@ -5,7 +5,7 @@ version=$(git tag --sort=committerdate --list '[0-9]*.[0-9]*'  | tail -n 1)
 commit=$(git rev-parse --short HEAD)
 if [ -n "${version}" ]; then
   version_commit=$(git rev-list -n 1 "${version}")
-  if [ "${git rev-parse HEAD}" == "${version_commit}" ]; then
+  if [ "$(git rev-parse HEAD)" == "${version_commit}" ]; then
     echo "${version}-${commit}" > dynamic-assets/version.txt
     exit 0
   fi
